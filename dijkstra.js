55c19aaa14f3c99b0e0dd4d045fd6b9b83e0e395
Ned Ruggeri <ruggeri@self-loop.com>
2018-03-15 18:05:24 -0700

    start out dijkstra demo!

---
+++ dijkstra.js
@@ -0,0 +1,116 @@
+class Vertex {
+  constructor(name) {
+    this.name = name;
+    this.edges = []
+  }
+
+  edgeVertexPairs() {
+    return this.edges.map(e => [
+      e, e.otherVertex(this)
+    ]);
+  }
+}
+
+class Edge {
+  constructor(name, cost, vertex1, vertex2) {
+    this.name = name;
+    this.cost = cost;
+    this.vertices = [vertex1, vertex2];
+
+    vertex1.edges.push(this);
+    vertex2.edges.push(this);
+  }
+
+  otherVertex(vertex1) {
+    if (this.vertices[0] == vertex1) {
+      return this.vertices[1];
+    } else if (this.vertices[1] == vertex1) {
+      return this.vertices[0];
+    } else {
+      throw "Vertex not at either end!"
+    }
+  }
+}
+
+class ResultEntry {
+  constructor(toVertex, lastEdge, totalCost) {
+    this.toVertex = toVertex;
+    this.lastEdge = lastEdge;
+    this.totalCost = totalCost;
+  }
+}
+
+// Ideally would be a heap.
+class Fringe {
+  constructor() {
+    this.store = new Map();
+  }
+
+  extractMinimumEntry() {
+    let minimumEntry = null;
+
+    this.store.forEach(entry => {
+      if (!minimumEntry || minimumEntry.totalCost < entry.totalCost) {
+        minimumEntry = entry;
+      }
+    });
+
+    this.store.delete(minimumEntry.toVertex);
+
+    return minimumEntry;
+  }
+
+  addEntry(toVertex, edge, totalCost) {
+    const currentEntry = this.store.get(toVertex);
+    if (!currentEntry || totalCost < currentEntry) {
+      this.store.set(toVertex, new ResultEntry(
+        toVertex,
+        edge,
+        totalCost
+      ));
+    }
+  }
+
+  isEmpty() {
+    return this.store.size == 0;
+  }
+}
+
+function dijkstra(startVertex) {
+  const result = new Map();
+  const fringe = new Fringe();
+  fringe.addEntry(
+    startVertex,
+    null,
+    0
+  );
+
+  while (!fringe.isEmpty()) {
+    const entry = fringe.extractMinimumEntry();
+    result.set(entry.toVertex, entry);
+
+    for (const evPair of entry.toVertex.edgeVertexPairs()) {
+      const [edge, newVertex] = evPair;
+
+      if (result.has(newVertex)) {
+        continue;
+      }
+
+      const newTotalCost = entry.totalCost + edge.cost;
+      fringe.addEntry(newVertex, edge, newTotalCost);
+    }
+  }
+
+  return result;
+}
+
+const v1 = new Vertex('v1');
+const v2 = new Vertex('v2');
+const v3 = new Vertex('v3');
+
+const e1 = new Edge('e1', 1, v1, v2);
+const e2 = new Edge('e2', 2, v2, v3);
+const e3 = new Edge('e3', 2, v3, v1);
+
+const result = dijkstra(v1);
+console.log(result);
