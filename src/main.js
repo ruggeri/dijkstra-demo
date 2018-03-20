019d45e4cb12c2819831ca93d926f3cbd432bfcb
Ned Ruggeri <ruggeri@self-loop.com>
2018-03-20 16:07:52 -0700

    document main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+const { generateGraph } = require('./support/graphGeneration');
+const handleMessage = require('./ui/dijkstraMessageHandler');
+
+// Generate the graph.
 const {
   vertices,
   vertexPositions,
@@ -5,24 +9,29 @@ const {
   goalVertex
 } = generateGraph();
 
+// Get various DOM elements.
 const canvasEl = document.getElementById('graph-canvas');
 const fringePreEl = document.getElementById('fringe-pre');
 const resultPreEl = document.getElementById('result-pre');
 const actionPreEl = document.getElementById('action-pre');
 
+// Build a graph viewer for the canvas.
 const graphViewer = new GraphViewer(
   canvasEl, vertices, vertexPositions, new DijkstraGraphColorer(),
 );
 graphViewer.draw();
 
+// Run Dijkstra's algorithm and collect up all messages.
 const dijkstraMessages = Array.from(
   dijkstra(startVertex, (vertex) => heuristic(vertex, goalVertex))
 );
 
-let msgIndex = 0;
+// Draw initial state of the graph.
 handleMessage(dijkstraMessages[0], startVertex, goalVertex);
-const numMessages = dijkstraMessages.length;
 
+// Let user iterate backward/forward through the messages.
+let msgIndex = 0;
+const numMessages = dijkstraMessages.length;
 document.addEventListener('keypress', (e) => {
   if (e.key === 'j') {
     msgIndex += (msgIndex < (numMessages - 1)) ? 1 : 0;
